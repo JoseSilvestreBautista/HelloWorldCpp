9aa257f4176d0500699b08c6bd334caced4daa3b
Jose Silvestre-Bautista <43448715+JoseSilvestreBautista@users.noreply.github.com>
2019-05-16 17:08:10 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout << "Hello User!\n" << endl;
+
+    cout << "Production Line Tracker\n" << endl;
+
+    cout << "1. Add Employee Account" << endl;
+    cout << "2. Add Music Player" << endl;
+    cout << "3. Add Movie Player" << endl;
+    cout << "4. Display Production Statistics" << endl;
+    cout << "5. Exit" << endl;
 }
