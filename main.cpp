c4b630b344ac766fb56e0a0c74a87a99bfa4f078
Jose Silvestre-Bautista <43448715+JoseSilvestreBautista@users.noreply.github.com>
2019-05-24 21:57:26 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,26 @@
 #include <string>
 
 // these are prototypes
-void menu(); // display main menu
+/**
+ * @brief  Is the main menu display for employees.
+ */
+void menu();
 
-void processing(int); // prevents user from invalid choice in main menu
+/**
+ * @brief Process the user's main menu selection and prevents improper selection.
+ * @param Takes the user's number input for main menu selection: userMenuSelection.
+ */
+void processing(int);
 
-int produceItems(); // next menu from choosing 1. from main menu also serial number for products
+/**
+ * @brief  Function that records products made using serial numbers.
+ * The result of choosing produce items from main menu.
+ */
+int produceItems();
 
+/**
+ * @brief Item type menu from produceItems function.
+ */
 void itemTypeMenu(); // item display menu. it was created to display several times if user has invalid selection
 
 int main() {  // drives the entire program by calling other functions.
@@ -25,13 +39,12 @@ int main() {  // drives the entire program by calling other functions.
     menu(); // call to the main menu to display
     int userMenuSelection; // call to the main menu
     do { // ask user to select from the main menu and prevents them from choosing invalid selections
-        std::cin >> userMenuSelection;
+        std::cin
+                >> userMenuSelection;// the loop should be removed to its own function to keep main clean and efficient.
         processing(userMenuSelection);
     } while (userMenuSelection > 6 || userMenuSelection < 1);
-
     return 0;
 }
-
 
 void menu() { // main menu display
     std::cout << "Production Line Tracker\n" << std::endl;
@@ -43,7 +56,7 @@ void menu() { // main menu display
     std::cout << "6. Exit" << std::endl;
 }
 
-void stub() { // dummy function used as a temporary call for future function not built
+void stub() { // dummy function used as a temporary call for future function not yet built.
     std::cout << "I am the stub" << std::endl;
 }
 
@@ -60,7 +73,7 @@ void processing(int userMenuSelection) { // main menu decision function. passes 
         stub();
     } else if (userMenuSelection == 6) {
         std::cout << "Exit" << std::endl;
-    } else {
+    } else { // may fail if user types letters. Fix this issue
         std::cout << "Not a valid selection" << std::endl;
         menu();
     }
@@ -70,7 +83,7 @@ int produceItems() { // submenu from selecting 1. Produce Items from main menu
     // Eventually the user will be able to choose the item to produce.
     // For now, just have them input the information.
     std::cout << "Enter the Manufacturer\n";
-    std::string manufacturer;
+    std::string manufacturer; // Ask for the companies name
     std::cin >> manufacturer;
     std::cout << "Enter the Product Name\n";
     std::string prodName;
@@ -89,7 +102,7 @@ int produceItems() { // submenu from selecting 1. Produce Items from main menu
             itemTypeCode = "AM";
         } else if (itemTypeChoice == 4) {
             itemTypeCode = "VM";
-        } else {
+        } else { // falls to produce ItemTypeChoice if user selects invalid choice the first time. Fix issue.
             std::cout << "Invalid Input\n" << std::endl;
             itemTypeMenu();
             std::cin >> itemTypeChoice;
@@ -103,8 +116,8 @@ int produceItems() { // submenu from selecting 1. Produce Items from main menu
     // outputting production number and serial number
     // produces the serial number products
     int productNumber;
-    std::ofstream myfile("Production.txt", std::ios::app);
-    myfile.is_open();
+    std::ofstream myfile("Production.txt", std::ios::app); // This create the file Production.txt and appends data.
+    myfile.is_open();// opens the file
 
     for (productNumber = 0; productNumber < numProduced;) {
         ++productNumber;
@@ -114,10 +127,10 @@ int produceItems() { // submenu from selecting 1. Produce Items from main menu
 
         myfile << "Production Number: " << productNumber << " Serial Number: " << manufacturer.substr(0, 3)
                << itemTypeCode << std::setfill('0') << std::setw(5) << productNumber
-               << std::endl;
+               << std::endl; // this code chunk  writes the serial number to the Production.txt file.
 
     }
-    myfile.close();
+    myfile.close();// closes the Production.txt file
 
     return 0;
 }
@@ -129,3 +142,5 @@ void itemTypeMenu() { //  itemTypeMenu from produce items
               "3. AudioMobile\n" <<
               "4. VisualMobile\n";
 }
+
+
