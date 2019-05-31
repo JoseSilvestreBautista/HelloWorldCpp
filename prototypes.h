764e4d56e97d8995d0df2c8895ef778616ec3d21
Jose Silvestre-Bautista <43448715+JoseSilvestreBautista@users.noreply.github.com>
2019-05-31 00:06:06 -0400

    Update prototypes.h

--- prototypes.h
+++ prototypes.h
@@ -1,1 +1,53 @@
+//
+// Created by joses on 5/29/2019.
+//
+
+#ifndef HELLOWORLD_PROTOTYPES_H
+#define HELLOWORLD_PROTOTYPES_H
+
+// these are prototypes
+/**
+ * @brief  Is the main menu display for employees.
+ */
+void menu();
+
+/**
+ * @brief Process the user's main menu selection and prevents improper selection.
+ * @param Takes the user's number input for main menu selection: userMenuSelection.
+ */
+void processing(int, std::vector<std::string> &productLineManufacturer,
+                std::vector<std::string> &productLineName,
+                std::vector<std::string> &productLineItemType);
+
+/**
+ * @brief  Function that records products made using serial numbers.
+ * The result of choosing produce items from main menu.
+ */
+int produceItems(std::vector<std::string> &productLineManufacturer,
+                 std::vector<std::string> &productLineName,
+                 std::vector<std::string> &productLineItemType);
+
+/**
+ * @brief Item type menu from produceItems function.
+ */
+void itemTypeMenu(); // item display menu. it was created to display several times if user has invalid selection
+
+int menuSelection();
+
+void addToProductLine(std::vector<std::string> &productLineManufacturer,
+                      std::vector<std::string> &productLineName,
+                      std::vector<std::string> &productLineItemType);
+
+void processingNewProductInfo(std::vector<std::string> &productLineManufacturer,
+                              std::vector<std::string> &productLineName,
+                              std::vector<std::string> &productLineItemType);
+
+void newAvailableDetailedProducts(std::vector<std::string> &productLineManufacturer,
+                                  std::vector<std::string> &productLineName,
+                                  std::vector<std::string> &productLineItemType);
+
+
+void availableProductsDisplay(std::vector<std::string> &productLineName);
+
+#endif //HELLOWORLD_PROTOTYPES_H
 
